e1ec6efc80a051a9dc96ce5de3830003eda1e475
jacqueline-homan <jacquelinehoman7@gmail.com>
2017-02-26 03:54:11 -0500

    printing strings

---
+++ print2.hs
@@ -0,0 +1,9 @@
+module Print2 where
+
+main :: IO()
+main = do 
+    putStrLn "One two, Freddy's coming for you, "
+    putStr "three four, better lock your door, "
+    putStr "five six, grab your crucifix, "
+    putStr "seven eight, better stay up late, "
+    putStrLn "nine ten, never sleep again."
