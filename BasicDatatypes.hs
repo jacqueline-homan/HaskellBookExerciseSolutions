1b4565977518f3f6e72adf6ed1ed3b973c50287b
jacqueline-homan <jacquelinehoman7@gmail.com>
2017-03-14 14:14:55 -0400

    end of Ch 4 exeercise solutions

--- BasicDatatypes.hs
+++ BasicDatatypes.hs
@@ -24,3 +24,7 @@ isPalindrome y = y == reverse y
 
 myAbs :: Integer -> Integer
 myAbs x = if x < 0 then negate x else x 
+
+f :: (a, b) -> (c, d) -> ((b, d), (a, c))
+f x y = ((snd x, snd y), (fst x, fst y)) 
+
---
+++ Exercises4.73.hs
@@ -0,0 +1,22 @@
+module CorrectedSyntax where
+
+x :: Int -> Int -> Int
+x = (+)
+
+f :: Foldable t => t a -> Int
+f xs = w `x` 1
+    where w = length xs
+
+{- Here's hpw to correctly write the identity function
+x :: [a] -> [a]
+x = \x -> x 
+or just
+x = (\x -> x)
+
+In GHCi, you'd just enter (\x -> x) <value_assigned_to_x>
+-}
+
+(t : ts) = [1,2,3]
+
+g :: (a, b) -> a
+g (a, b) = a
