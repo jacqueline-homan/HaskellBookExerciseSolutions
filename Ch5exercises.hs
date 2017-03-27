405c4cadb7e09e34e75708f0bc93f248e49bc988
jacqueline-homan <jacquelinehoman7@gmail.com>
2017-03-27 00:27:18 -0400

    partial application

--- Ch5exercises.hs
+++ Ch5exercises.hs
@@ -5,3 +5,21 @@ addStuff a b = a + b + 5
 
 addTen = addStuff 5
 fifteen = addTen 5
+
+funcIgnoresArgs :: a -> a -> a -> String
+funcIgnoresArgs x y z = "Blah"
+
+nonsense :: Bool -> Integer
+nonsense True = 805
+nonsense False = 31337
+
+typicalCurriedFunction :: Integer -> Bool -> Integer
+typicalCurriedFunction i b = 
+	i + (nonsense b)
+
+uncurriedFunction :: (Integer, Bool) -> Integer
+uncurriedFunction (i, b) = 
+	i + (nonsense b)
+
+anonymous :: Integer -> Bool -> Integer
+anonymous = \i b -> i + (nonsense b)
